2bfa029e3e5302b16a7cb1c6d57e6ea5c09cb5bb
Jonathan <jonathan15989@protonmail.com>
2023-12-23 20:58:02 -0300

    test: add test for created helper function

--- tests/shared/helpers/http.spec.ts
+++ tests/shared/helpers/http.spec.ts
@@ -1,8 +1,8 @@
-import { badRequest, ok } from "@helpers/http";
+import { badRequest, created, ok } from "@shared/helpers/http"
 
 describe("HTTP helper", () => {
   test("ensure ok helper function returns correct data", () => {
-    const response = ok("any_data");
+    const response = ok("any_data")
 
     expect(response).toEqual({
       statusCode: 200,
@@ -10,18 +10,32 @@ describe("HTTP helper", () => {
         status: "success",
         data: "any_data",
       },
-    });
-  });
+    })
+  })
 
   test("ensure badRequest helper function returns correct data", () => {
-    const response = badRequest("any_message");
+    const error = new Error("any_message")
+    const response = badRequest(error)
 
     expect(response).toEqual({
       statusCode: 400,
       body: {
         status: "error",
         message: "any_message",
+        error,
       },
-    });
-  });
-});
+    })
+  })
+
+  test("ensure created helper function returns correct data", () => {
+    const response = created()
+
+    expect(response).toEqual({
+      statusCode: 201,
+      body: {
+        status: "success",
+        data: null,
+      },
+    })
+  })
+})
